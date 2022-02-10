abbb0bd9c42f6b8bcd8a18ef2d3ff7783f5dd593
Joel Leal Jr <71736039+joellealjj@users.noreply.github.com>
2022-02-10 11:05:07 -0300

    integracao com storage firebase (#3)
    Co-authored-by: Joel Leal Jr <indra.jcljunior@voegol.com.br>

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import storage from '@react-native-firebase/storage';
+import { Alert } from 'react-native';
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
@@ -9,6 +11,8 @@ import { Container, Content, Progress, Transferred } from './styles';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,24 +30,58 @@ export function Upload() {
     }
   };
 
+  // enviando imagens para o storage do firebase
+  async function handleUpload() {
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/images/${fileName}.png`);
+
+    /*
+    // fazer upload de forma simples
+    reference
+      .putFile(image)
+      .then(() => Alert.alert('Upload concluído!'))
+      .catch((error) => console.error(error));
+    */
+
+    // envia o arquivo pegando a uri do arquivo (image)
+    const uploadTask = reference.putFile(image);
+
+    // fica ouvindo o progresso do upload
+    uploadTask.on('state_changed', taskSnapshot => {
+      const percent = ((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100).toFixed(0);
+      setProgress(percent);
+
+      setBytesTransferred(`${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`);
+    });
+
+    // tratando sucesso
+    uploadTask.then(async () => {
+      // salvar no banco a url da imagem para ser mais rápido para listar na outra tela
+      const imageUrl = await reference.getDownloadURL();
+      console.log('joeeel', imageUrl);
+      
+      Alert.alert('Upload concluído com sucesso!');
+    });
+  }
+
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
         <Button
           title="Fazer upload"
-          onPress={() => { }}
+          onPress={handleUpload}
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
